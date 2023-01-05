e47b01f56f4c8849de7a4bdafb2cce90325ca15b
Niral Patel <niral0901@gmail.com>
2023-01-05 17:17:47 +0530

    check CI status

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -131,7 +131,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
 
 STATICFILES_DIRS = [
-    BASE_DIR/"static",
+    BASE_DIR / "static",
 ]
 
 # Default primary key field type
